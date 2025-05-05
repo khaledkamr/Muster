a88318d563f06cc9cd4fff2f3ce6468cb4ec9536
Khaled Kamr <200013171@must.edu.eg>
2025-05-05 22:23:08 +0300

    logout Modal

--- resources/views/layouts/professor.blade.php
+++ resources/views/layouts/professor.blade.php
@@ -230,9 +230,9 @@
 
             <!-- Logout -->
             <li class="nav-item">
-                <form action="{{ route('logout') }}" method="POST">
+                <form action="{{ route('logout') }}" method="POST" id="logout-form">
                     @csrf
-                    <button type="submit" class="text">
+                    <button type="button" class="text" data-bs-toggle="modal" data-bs-target="#logoutModal">
                         <i class="bi bi-box-arrow-right"></i> Logout
                     </button>
                 </form>
@@ -263,6 +263,24 @@
     <!-- Content -->
     <div class="content">
         @yield('content')
+    </div>
+
+    <div class="modal fade" id="logoutModal" tabindex="-1" aria-labelledby="logoutModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title text-dark fw-bold" id="logoutModalLabel">Confirm Logout</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body text-dark">
+                    Are you sure you want to logout?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-danger" id="confirmLogout">Logout</button>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Bootstrap JavaScript -->
@@ -317,6 +335,15 @@
                     });
                 }
             }
+        });
+
+        document.addEventListener('DOMContentLoaded', function () {
+            const logoutForm = document.getElementById('logout-form');
+            const confirmLogoutButton = document.getElementById('confirmLogout');
+
+            confirmLogoutButton.addEventListener('click', function () {
+                logoutForm.submit(); 
+            });
         });
     </script>
 </body>
